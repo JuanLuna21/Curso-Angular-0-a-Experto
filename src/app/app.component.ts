c75e7572233b865758f013e7c0397cefdc6a08cd
JuanLuna21 <xjuanigancio@gmail.com>
2022-09-27 20:54:47 -0300

    Creando Modulos manuales.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // nombre del componente.
+  templateUrl: './app.component.html', // contraparte de html.  la diferencia con el template solo, es solamente un literal. por ejemplo <h1></h1>
+  styleUrls: ['./app.component.css'] // contraparte css
 })
 export class AppComponent {
-  title = 'bases';
+
+
 }
